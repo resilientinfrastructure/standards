6e610c709462a24aeae18a2edcdabf5ce27acf0e
Manrique Vargas <mv1742@nyu.edu>
2024-05-18 14:37:49 -0400

    Update and rename pandora's_schema.py to panderas_schema.yml

--- pandora's_schema.py
+++
@@ -1,4 +0,0 @@
-# class-based API
-class DemoSchema(pa.DataFrameModel):
-    item: Series[str] = pa.Field(isin=["apple", "orange"], coerce=True)
-    price: Series[float] = pa.Field(gt=0, coerce=True)
